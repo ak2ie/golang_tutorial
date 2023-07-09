b6ebd59637e44dfd0ab566282e89554adcd424be
ak2ie <mail@ak2ie.net>
2023-07-09 12:39:29 +0000

    ロギング

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,16 @@
 FROM golang:1.19  
   
-WORKDIR /app  
-RUN go install github.com/cosmtrek/air@latest  
-RUN go install github.com/go-delve/delve/cmd/dlv@latest  
+WORKDIR /app
+RUN go install github.com/cweill/gotests/gotests@v1.6.0
+RUN go install github.com/fatih/gomodifytags@v1.16.0
+RUN go install github.com/josharian/impl@v1.1.0
+RUN go install github.com/haya14busa/goplay/cmd/goplay@v1.0.0
+RUN go install github.com/go-delve/delve/cmd/dlv@latest
+RUN go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 RUN go install golang.org/x/tools/gopls@latest
-RUN go get -u github.com/go-chi/chi/v5
+RUN go install github.com/ramya-rao-a/go-outline@v0.0.0-20210608161538-9736a4bde949
+COPY go.mod go.sum ./
+RUN go mod download
 
 # Run Hot Reload
 CMD ["air", "-c", ".air.toml"]
--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ak2ie/golang_tutorial/cmd/hello"
 	middleware "github.com/deepmap/oapi-codegen/pkg/chi-middleware"
 	"github.com/go-chi/chi/v5"
+	"golang.org/x/exp/slog"
 )
 
 func main() {
@@ -17,5 +18,33 @@ func main() {
 	r.Use(middleware.OapiRequestValidator(swagger))
 	h := adapters.NewServer()
 	hello.HandlerFromMux(h, r)
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", MiddlewareLogging(r)))
+}
+
+// ロギング
+func MiddlewareLogging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("start", "URL", r.URL)
+		lrw := NewLoggingResposeWriter(w)
+
+		next.ServeHTTP(lrw, r)
+
+		statusCode := lrw.statusCode
+		slog.Info("end", "URL", r.URL, "StatusCode", http.StatusText(statusCode))
+	})
+}
+
+// ResponseWriterを満たす
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewLoggingResposeWriter(w http.ResponseWriter) *loggingResponseWriter {
+	return &loggingResponseWriter{w, http.StatusOK}
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code // ステータスコードを保存
+	lrw.ResponseWriter.WriteHeader(code)
 }
